2b8f1ff015a07b3686a4c54ab43c34419e687b84
lyg0114 <whdnseowkd@naver.com>
2023-07-08 11:50:33 +0900

    Add : add querydsl-study

---
+++ querydsl-study/src/test/java/com/study/querydslstudy/QuerydslStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.querydslstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuerydslStudyApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
