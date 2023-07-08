16bb2c1864dfb9b666265a41c7fa4cd5fd34ff98
lyg0114 <whdnseowkd@naver.com>
2023-07-08 14:12:49 +0900

    Add : add spring-data-jpa-study

---
+++ spring-data-jpa-study/src/main/java/com/study/springdatajpastudy/SpringDataJpaStudyApplication.java
@@ -0,0 +1,13 @@
+package com.study.springdatajpastudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataJpaStudyApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringDataJpaStudyApplication.class, args);
+  }
+
+}
