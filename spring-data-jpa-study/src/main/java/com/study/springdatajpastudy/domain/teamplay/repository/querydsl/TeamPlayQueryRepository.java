1f1ec8f766651de314111969ee26e38e969f2711
lyg0114 <whdnseowkd@naver.com>
2023-07-10 13:40:31 +0900

    Add : TeamPlayQueryRepository

---
+++ spring-data-jpa-study/src/main/java/com/study/springdatajpastudy/domain/teamplay/repository/querydsl/TeamPlayQueryRepository.java
@@ -0,0 +1,17 @@
+package com.study.springdatajpastudy.domain.teamplay.repository.querydsl;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+/**
+ * @author : iyeong-gyo
+ * @package : com.study.springdatajpastudy.domain.teamplay.repository.querydsl
+ * @since : 2023/07/10
+ */
+@RequiredArgsConstructor
+@Repository
+public class TeamPlayQueryRepository {
+
+  private final JPAQueryFactory queryFactory;
+}
