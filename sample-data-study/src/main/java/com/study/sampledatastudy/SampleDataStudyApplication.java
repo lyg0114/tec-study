9f5857a4f9cdb0def0e7ba62a0b8f453e51cbe3e
lyg0114 <whdnseowkd@naver.com>
2023-07-10 13:08:18 +0900

    Add : add new project

---
+++ sample-data-study/src/main/java/com/study/sampledatastudy/SampleDataStudyApplication.java
@@ -0,0 +1,13 @@
+package com.study.sampledatastudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleDataStudyApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SampleDataStudyApplication.class, args);
+  }
+
+}
---
+++ sample-data-study/src/test/java/com/study/sampledatastudy/SampleDataStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.sampledatastudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleDataStudyApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
